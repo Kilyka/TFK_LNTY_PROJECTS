a4c91c51a46a00f7b56bcccf7b82a99a190c2411
Kilyka <158220404+Kilyka@users.noreply.github.com>
2024-10-14 17:41:18 +0300

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,9 @@
+name = "Данііл"
+surname = "Чвир"
+age = 17
+hw = "Hello World"
+
+print("Тип name:",type(name))
+print("Тип surname:", type(surname))
+print("Тип age:" ,type(age))
+print("Тип hw:", type(hw))
